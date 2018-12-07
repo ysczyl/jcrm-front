6fe74cdb5369affc547984959031ed8459d28b37
ysczyl <1239864102@qq.com>
2018-12-07 22:09:16 +0800

    添加市场活动

--- src/services/opportunity.js
+++ src/services/opportunity.js
@@ -116,11 +116,11 @@ export async function insertOpportunity(params) {
 export async function insertSource(params) {
     const token = sessionStorage.getItem('token');
     return request(`/server/opportunity/source`, {
+        headers: { Authorization: `Bearer ${token}` },
         method: 'POST',
         body: {
             ...params,
             method: 'post',
-            headers: { Authorization: `Bearer ${token}` },
         },
     });
 }
